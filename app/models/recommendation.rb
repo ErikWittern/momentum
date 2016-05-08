e2960782a9b2afa637082947b989093e6e4d1a8c
Christopher Young <ccyoung@us.ibm.com>
2016-05-07 22:11:50 -0400

    fix boogs

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,5 +2,9 @@ class Recommendation < ActiveRecord::Base
   belongs_to :user
   belongs_to :place
 
-  validates :category, inclusion: { in: %w(eat drink party explore), presence: true
+  BY_ROLES = %w(novice regular local)
+  CATEGORIES = %w(eat drink party explore)
+
+  validates :category, inclusion: { in: CATEGORIES }, presence: true
+  validates :by_role, inclusion: { in: BY_ROLES }, presence: true
 end
