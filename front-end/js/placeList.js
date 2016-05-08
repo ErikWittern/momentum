2a6953331659f11d61db8f55d4df6c45a61c7dca
Juergen Cito <citostyle@gmail.com>
2016-05-07 20:56:05 -0400

    Showing places

--- front-end/js/placeList.js
+++ front-end/js/placeList.js
@@ -5,7 +5,7 @@ var request = require('superagent')
 
 var PlaceList = React.createClass({
   getInitialState: function() {
-      return {data: []};
+      return {data: [], currentRecommendation: 0}
     },
 
   componentDidMount : function() {
@@ -13,23 +13,54 @@ var PlaceList = React.createClass({
       .get('/places.json')
       .end(function (err, res) {
         if (err) return
-        console.log(res)
-        this.setState({data: res.body});
+        console.log(res.body)
+        this.setState({data: res.body})
       }.bind(this))
   },
-  render: function() {
-        var placeNodes = this.state.data.map(function(place) {
-          return (
-            <Place name={place.name} key={place.name} />
-          );
-        });
-        return (
-          <div className="placeList">
-            {placeNodes}
-          </div>
-        );
+
+  handleSubmit: function(e) {
+      e.preventDefault();
+
+      var currentRecommendation = this.state.currentRecommendation
+
+      var newRecommendation;
+      if(currentRecommendation < this.state.data.length - 1) {
+        newRecommendation = currentRecommendation + 1
+      } else {
+        newRecommendation = 0
       }
 
+      this.setState({currentRecommendation: newRecommendation})
+    },
+
+  render: function() {
+      console.log(this.state.data)
+        // var placeNodes = this.state.data.map(function(place) {
+        //   return (
+        //     <Place name={place.name} key={place.name} />
+        //   );
+        // });
+        var place = this.state.data[this.state.currentRecommendation]
+        if(place == undefined) return null
+
+        return (
+          <form className="placeForm" onSubmit={this.handleSubmit}>
+            <Place name={place.name} key={place.name} />
+            <input type="submit" value={this.getNextButtonText()} />
+          </form>
+        );
+    },
+
+    getNextButtonText: function() {
+      var buttonText = ["Been there, done that", "I don't think so",
+                        "Um, no thanks", "Maybe another time",
+                        "Hm…no", "I'm not feeling it", "Nope",
+                        "Really?", "Let's roll the dice again",
+                        "Give me another", "Today ain't the day"]
+      var min = 0
+      var max = buttonText.length - 1
+      return buttonText[Math.floor(Math.random() * (max - min + 1)) + min];
+    }
 })
 
 export {PlaceList}
